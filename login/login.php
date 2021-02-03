78cab3befcd6246458d1e21bbce896021a78a86e
keishaputritheanny <pkeisha47@gmail.com>
2021-02-03 14:06:24 +0700

    innitial commit

---
+++ login/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+    <label for="username">username : </label>
+    <input type="text" name="username" id="username">
+    <p>
+    <label for="password">password : </label>
+    <input type="text" name="password" id="password">
+    <p>
+    <button type="submit" name="submit">Login</button>
+    </form>
+</body>
+</html>
