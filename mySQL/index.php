d617d51f342ad2e2037b2e8b8ac0a7891e807312
keishaputritheanny <pkeisha47@gmail.com>
2021-02-17 12:11:46 +0700

    mysql

---
+++ mySQL/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Daftar Siswa</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Gambar</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+            <th>Email</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td><img src="img/ivanmartinez.jpeg" width="100"></td>
+            <td>Ivan José Martínez Pérez</td>
+            <td>XI RPL A</td>
+            <td>ivanmartinez@gmail.com</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td><img src="img/luispatridge.jpeg" width="100"></td>
+            <td>Louis Partridge</td>
+            <td>XI RPL A</td>
+            <td>louispatridge@gmail.com</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td><img src="img/mikeymurphy.jpeg" width="100"></td>
+            <td>Michael J. Murphy</td>
+            <td>XI RPL B</td>
+            <td>mikeymurphy@gmail.com</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td><img src="img/herofiennes-tiffin.jpeg" width="100"></td>
+            <td>Hero Beauregard Faulkner Fiennes Tiffin</td>
+            <td>XI RPL D</td>
+            <td>herofinneastiffin@gmail.com</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td><img src="img/harveycantwell.jpeg" width="100"></td>
+            <td>Harvey Leigh Cantwell</td>
+            <td>XI RPL D</td>
+            <td>harveycantwell@gmail.com</td>
+        </tr>
+    </table>
+</body>
+
+</html>
